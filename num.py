16b293987b2a8a16378b850b3e01db18b21c45aa
alifurrahman53@gmail.com <alifurrahman53@gmail.com>
2022-01-25 15:22:44 +0000

    hi

---
+++ num.py
@@ -0,0 +1,5 @@
+x=0.2+0.1
+print(x)
+# f-string
+str=f"{x:.2f}"
+print(str)
--- week-1.py
+++ week-1.py
@@ -1,2 +1,25 @@
-for x in range (10):
- print("hello world")
+from tkinter import TRUE
+
+
+li=['Andrew','Alif','Harry','Ryan','Wheezy','Kevan']
+print(li[0])
+print(li[1])
+print(li[2])
+print(li[3])
+print(li[4])
+print(li[5])
+n=len(li)
+print(n)
+print(f'{li[0].lower(),"Go back Man"}, "{li[2].upper()}"!"Dont Worry"')
+place=['CoxsBazar','Kashmir','Mount Averaste','Narikel Jingira','Nayagra']
+place.sort(reverse=TRUE)
+print(place)
+print(len(place))
+place.append('Switzarland')
+print(place)
+place.remove('Switzarland')
+print(place)
+place.reverse()
+print(place)
+place.remove(place[3])
+print(place)
